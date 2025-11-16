e3c39d4c686d08d953705a32c5f12c04e4ef4bcc
Bevan Tan <btan400@gmail.com>
2025-11-16 21:36:43 +1100

    fetchParkingAPI tests for 404 server error, 500 server errors, blank/null data

--- services/fetchParkingAPI.js
+++ services/fetchParkingAPI.js
@@ -1,6 +1,16 @@
 //grabs data from the parking API
 export async function fetchParkingAPI(){
-        const response = await fetch('https://data.melbourne.vic.gov.au/api/explore/v2.1/catalog/datasets/on-street-parking-bay-sensors/exports/json?lang=en&timezone=Australia%2FSydney');
+        const response = await fetch("https://data.melbourne.vic.gov.au/api/explore/v2.1/catalog/datasets/on-street-parking-bay-sensors/exports/json?lang=en&timezone=Australia%2FSydney");
 
-        return await response.json();
+        if (!response.ok){
+            throw new Error(`Fetching API Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (!data || data.length === 0){
+            throw new Error("API returned no parking data")
+        }
+
+        return data;
 }
--- services/fetchParkingAPI.test.js
+++ services/fetchParkingAPI.test.js
@@ -4,37 +4,83 @@ import {expect, jest, test} from "@jest/globals";
 //testing fetchParkingAPI
 /*
 TESTING:
-- No data received - Error
-- Correct table headers (structure)
-- Incorrect datatype in column - Error
-- JSON file received???
+- ~Data is able to be received
+- ~Errors 404, 500
+- No data received - blank json
+- invalid Json
+- Correct table headers (structure) - move to saveData
+- Incorrect datatype in column - Error - move to SaveData
 */
 
 global.fetch = jest.fn();
 
-const mockData = [
-        "lastupdated":"2025-05-08T05:44:34+00:00",
-        "status_timestamp":"2025-04-14T03:01:40+00:00",
-        "zone_number":7218,
-        "status_description":"Unoccupied",
-        "kerbsideid":57940,
-        "location":{
-        "lon":144.95458322910955,
-        "lat":-37.82027020009988
-        }
-    ]
+// const mockData = [{
+//         "lastupdated":"2025-05-08T05:44:34+00:00",
+//         "status_timestamp":"2025-04-14T03:01:40+00:00",
+//         "zone_number":7218,
+//         "status_description":"Unoccupied",
+//         "kerbsideid":57940,
+//         "location":{
+//         "lon":144.95458322910955,
+//         "lat":-37.82027020009988
+//         }
+//     }]
 
-test("testing 1", async () => {
-    fetch.mockResolvedValueOnce({ json: async () => ["data1"] });
-    const data = await fetchParkingAPI();
-    expect(data).toEqual(["data1"]); 
+beforeEach(() => {
+    fetch.mockClear();
 });
 
-test("Error on no data recieved", async () => {
-    //fetching data, that is blank/null/empty
-    //should receive error - "Error 404 not found"
+test("Receiving Data", async () => {
+    fetch.mockResolvedValueOnce({ 
+        ok: true,
+        status: 200,
+        json: async () => ["testData1"] 
+    });
 
-    fetch.mockResolvedValueOnce({json: async () => [""]});
     const data = await fetchParkingAPI();
-    expect(data).reject.toThrow("Error 404")
+    expect(data).toEqual(["testData1"]); 
+});
+
+test("Error 404 API Not Found", async () => {
+    //fetching data, that is blank/null/empty
+    //should receive error
+
+    fetch.mockResolvedValueOnce({
+        ok: false,
+        status: 404
+    });
+
+    await expect(fetchParkingAPI()).rejects.toThrow("Fetching API Error: 404");
+});
+
+test("Error 500 API Interal Server Error", async () => {
+    //fetching data, that is blank/null/empty
+    //should receive error
+
+    fetch.mockResolvedValueOnce({
+        ok: false,
+        status: 500
+    });
+
+    await expect(fetchParkingAPI()).rejects.toThrow("Fetching API Error: 500");
+});
+
+test("Error - Receiving Blank Array", async () => {
+    fetch.mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        json: async () => []
+    })
+
+    await expect(fetchParkingAPI()).rejects.toThrow("API returned no parking data")
+});
+
+test("Error - Receiving null", async () => {
+    fetch.mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        json: async () => null
+    })
+
+    await expect(fetchParkingAPI()).rejects.toThrow("API returned no parking data")
 });
