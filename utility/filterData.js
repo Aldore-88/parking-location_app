221a55241f57144ec3a7e0c8c4647a311fbeab85
Bevan Tan <btan400@gmail.com>
2025-12-09 16:23:20 +1100

    working on filterData.js - to filter out saved data to then be used for map marker display

---
+++ utility/filterData.js
@@ -0,0 +1,19 @@
+import fs from "fs/promises"
+
+// simplify data to only the keys needed
+
+export async function filterData(filepath){
+    const rawData = await fs.readFile('./parking-data.json', 'utf-8');
+    const data = JSON.parse(rawData);
+
+
+    const filteredData = data
+    .filter(obj => obj.status_description === "Unoccupied")
+    .map(obj => ({
+        lat: obj.location.lat,
+        lon: obj.location.lon,
+        id: obj.kerbsideid
+    }));
+
+    return filteredData;
+}
